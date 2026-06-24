2f875b5395f97d75ef4c795260d4d8671f8ba7d7
ianchang <iy2chang@gmail.com>
2026-06-24 16:38:36 +0900

    expandable text

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import { useState } from "react";
-import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
+import ExpandableText from "./components/ExpandableText";
 
 function App() {
-  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
-
   return (
     <div>
-      <NavBar cartItemsCount={cartItems.length} />
-      <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
+      <ExpandableText maxChars={10}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit harum
+        quae quod minus placeat, excepturi beatae aliquam sint expedita, odio
+        nam? Praesentium minus nisi maiores optio dicta animi id nostrum
+        accusamus, asperiores odit, inventore iure quia hic autem nemo alias
+        incidunt dignissimos quisquam cumque quam consectetur sed! Nisi dolor,
+        fugiat quae dolorum, odio cum illo expedita suscipit velit illum
+        sapiente! Facilis velit, illo consectetur accusantium veniam quam
+        laboriosam necessitatibus vero explicabo beatae aut, odit, enim est
+        omnis. Expedita quo laborum voluptas vero mollitia error! A reiciendis
+        ipsa nulla nisi, soluta ipsum, beatae repellendus excepturi nobis
+        corrupti iure repudiandae temporibus perferendis?
+      </ExpandableText>
     </div>
   );
 }
---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+interface Props {
+  children: string;
+  maxChars?: number;
+}
+
+const ExpandableText = ({ children, maxChars = 100 }: Props) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  if (children.length <= 100) return <p>{children}</p>;
+
+  const text = isExpanded ? children : children.substring(0, maxChars);
+  return (
+    <p>
+      {text}...
+      <button onClick={() => setIsExpanded(!isExpanded)}>
+        {isExpanded ? "Less" : "More"}
+      </button>
+    </p>
+  );
+};
+
+export default ExpandableText;
