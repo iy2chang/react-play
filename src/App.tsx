927c07ab43d2e95d2a87fd16e4b886a601ef8374
ianchang <iy2chang@gmail.com>
2026-06-24 15:25:33 +0900

    add immer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
-import Like from "./components/Like";
+import { useState } from "react";
+import produce from "immer";
 
 function App() {
+  const [bugs, setBugs] = useState([
+    {
+      id: 1,
+      title: "Bug 1",
+      fixed: false,
+    },
+    {
+      id: 2,
+      title: "Bug 2",
+      fixed: false,
+    },
+  ]);
+
+  const handleClick = () => {
+    setBugs(
+      produce((draft) => {
+        const bug = draft.find((bug) => bug.id === 1);
+        if (bug) bug.fixed = true;
+      }),
+    );
+  };
+
   return (
     <div>
-      <Like onClick={() => console.log("clicked")} />
+      {bugs.map((bug) => (
+        <p key={bug.id}>
+          {bug.title} {bug.fixed ? "Fixed" : "New"}
+        </p>
+      ))}
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
