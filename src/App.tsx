1f4d3db1a039ba48cc1a777a09409d53991db89b
ianchang <iy2chang@gmail.com>
2026-06-30 05:13:35 +0900

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import ExpandableText from "./components/ExpandableText";
-import Form from "./components/Form";
-
+import { useEffect, useState } from "react";
+import ProductList from "./components/ProductList";
 function App() {
-  return <Form />;
+  const [category, setCategory] = useState("");
+
+  useEffect(() => {
+    document.title = "My App";
+  });
+
+  return (
+    <div>
+      <select
+        className="form-select"
+        onChange={(event) => setCategory(event.target.value)}
+      >
+        <option value=""></option>
+        <option value="Clothing">Clothing</option>
+        <option value="HouseHold">HouseHold</option>
+      </select>
+      <ProductList category={category} />
+    </div>
+  );
 }
 
 export default App;
